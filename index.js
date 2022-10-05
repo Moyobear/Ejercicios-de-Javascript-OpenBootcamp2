3ac4cc149ed193b1c38caca1d07897a08a105910
Jonathan Rodriguez <rodriguezjmm@hotmail.com>
2022-10-04 22:38:25 -0400

    Ejercicio 2 finalizado

---
+++ index.js
@@ -0,0 +1,10 @@
+const nombre = "Jonathan Rodríguez";
+const edad = 39;
+const developer = true;
+const birthday = new Date(1983, 3, 5);
+const libro = {
+  titulo: "Tus zonas erróneas",
+  autor: "Wayne Dyer",
+  fecha: 1976,
+  url: "https://es.wikipedia.org/wiki/Tus_zonas_err%C3%B3neas",
+};
