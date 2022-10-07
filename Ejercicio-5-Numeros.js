e9a86274ba3e383353859e90ee6fe1de08ffc7c6
Jonathan Rodriguez <rodriguezjmm@hotmail.com>
2022-10-07 10:52:18 -0400

    Ejercicio 5 de números Terminado

---
+++ Ejercicio-5-Numeros.js
@@ -0,0 +1,9 @@
+let alturaCm = 183;
+let alturaMts = 1.83;
+let pesoKg = 110.9;
+let alturaRound = Math.ceil(alturaMts);
+console.log(alturaRound);
+let pesoKgFloor = Math.floor(pesoKg);
+console.log(pesoKgFloor);
+let maxValue = Number.MAX_VALUE + 1 === Number.MAX_VALUE ? true : false;
+console.log(maxValue);
