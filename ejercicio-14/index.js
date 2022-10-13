26fdc0dd8cfa7f78d02d326aff8ad238f5869d8c
Jonathan Rodriguez <rodriguezjmm@hotmail.com>
2022-10-13 16:27:15 -0400

    Ejercicio Tema 14 Terminado

---
+++ ejercicio-14/index.js
@@ -0,0 +1,12 @@
+$("document").ready(() => {
+  //const boton = document.getElementById("btn");
+
+  //   boton.addEventListener("click", () => {
+  //     console.log("Aprendo rápido");
+  //     alert("Click en el botón");
+  //   });
+
+  $("#btn").click(() => {
+    console.log("Hola, estoy utilizando jQuery");
+  });
+});
