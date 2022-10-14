50b11c40f76cb2a2a0c2914ae7eb7f3301332676
Jonathan Rodriguez <rodriguezjmm@hotmail.com>
2022-10-14 13:29:58 -0400

    Ejercicio Tema 15 Terminado

---
+++ Ejercicio Tema 15/index.js
@@ -0,0 +1,15 @@
+const nombre = "Jonathan";
+const apellido = "Rodríguez";
+
+const persona = {
+  nombre: nombre,
+  apellido: apellido,
+};
+//console.log(persona);
+
+// localStorage.setItem("persona", JSON.stringify(persona));
+
+// sessionStorage.setItem("persona", JSON.stringify(persona));
+// let expires = new Date(Date.now() + 120 * 1000).toUTCString();
+
+// document.cookie = "objetoCookie=persona;expires=" + expires;
